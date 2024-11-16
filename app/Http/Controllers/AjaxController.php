8c91fe08f907f5c34451d455b81304ae1122bd44
Vivek Kumar <vivek.kumar1@provabmail.com>
2024-11-16 17:17:11 +0530

    main item marking with ajax sample

---
+++ app/Http/Controllers/AjaxController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Config;
+use App\Models\XoProducts;
+
+class AjaxController extends Controller
+{
+
+public function postdata(Request $request){
+        $validator = Validator::make($request->input(), [
+            'item_name' => 'required|string',
+            'item_code' => 'required|digits:5',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'code' => 400,
+                'status' => 'error',
+                'message' => $validator->errors()->first()
+            ], 400);
+        }
+
+        $req = $request->only('item_name', 'item_code');
+
+        // operations need to be performed on data here only 
+
+        if($req){
+            return response()->json([
+                'code' => 200,
+                'status' => 'success',
+                'message' => 'Data available!!!',
+                'data' => $req
+            ], 200);
+        }else{
+            return response()->json([
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'Data not available!!!'
+            ], 400);            
+        }
+    }
+
+
+
+    public function finditemvariants(){
+
+        $data = XoProducts::select(['baseitemnum as bin', 'id','itemnum as in'])
+                            ->distinct('baseitemnum')
+                            ->get()
+                            ->chunk(5) // shopify main product searching chunk size ==> take from env 
+                            ->toarray();
+        
+        $allIds = [];
+
+        foreach($data as $d){
+            foreach($d as $key=>$val){
+                if($val['bin'] == $val['in']){
+                    array_push($allIds, $val['id']);
+                }
+            }
+        }
+
+        // marking all the matching products as main 
+        foreach($allIds as $id){
+            XoProducts::where(['id' => $id])
+                        ->update([
+                            'is_main' => 1
+                        ]);
+        }
+
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AjaxController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/postdata', [AjaxController::class, 'postdata'])->name('postdata');
+
+Route::get('/findmainitemandvariants', [AjaxController::class, 'finditemvariants'])->name('finditemvariants');
